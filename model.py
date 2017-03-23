bee82c2092d8c6607b2a2d4972ae16b19fb138b0
WenchenLi <largemaths@gmail.com>
2017-03-23 14:59:49 -0500

    add descriptions for each script

--- model.py
+++ model.py
@@ -1,6 +1,22 @@
+# Copyright 2017 The Wenchen Li. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
 """
 Replicate "Very Deep Convolutional Networks for Natural Language Processing" by Alexis Conneau,
 Holger Schwenk, Yann Le Cun, Loic Barraeau, 2016
+https://arxiv.org/pdf/1606.01781.pdf
 
 New NLP architecture:
 1. Operate at lowest atomic representation of text (characters)
