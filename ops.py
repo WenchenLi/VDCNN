6873fa2a3befd3a92fca01bde82e5cc117ea075f
WenchenLi <largemaths@gmail.com>
2017-03-27 16:40:28 -0500

    plan to move ops into model.py

--- ops.py
+++ ops.py
@@ -1,97 +1,97 @@
-# Copyright 2017 The Wenchen Li. All Rights Reserved.
+# # Copyright 2017 The Wenchen Li. All Rights Reserved.
+# #
+# # Licensed under the Apache License, Version 2.0 (the "License");
+# # you may not use this file except in compliance with the License.
+# # You may obtain a copy of the License at
+# #
+# #     http://www.apache.org/licenses/LICENSE-2.0
+# #
+# # Unless required by applicable law or agreed to in writing, software
+# # distributed under the License is distributed on an "AS IS" BASIS,
+# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# # See the License for the specific language governing permissions and
+# # limitations under the License.
+# # ==============================================================================
+# """
+# special operations used in VDCNN,
+# k_max_pool
+# """
+# import tensorflow as tf
+# import config
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# def _to_tensor(x, dtype):
+#     x = tf.convert_to_tensor(x)
+#     if x.dtype != dtype:
+#         x = tf.cast(x, dtype)
+#     return x
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""
-special operations used in VDCNN,
-k_max_pool
-"""
-import tensorflow as tf
-import config
-
-
-def _to_tensor(x, dtype):
-    x = tf.convert_to_tensor(x)
-    if x.dtype != dtype:
-        x = tf.cast(x, dtype)
-    return x
-
-
-def _normalize_axis(axis, ndim):
-    if type(axis) is tuple:
-        axis = list(axis)
-    if type(axis) is list:
-        for i, a in enumerate(axis):
-            if a is not None and a < 0:
-                axis[i] = a % ndim
-    else:
-        if axis is not None and axis < 0:
-            axis = axis % ndim
-    return axis
-
-
-def ndim(x):
-    '''Returns the number of axes in a tensor, as an integer.
-    '''
-    dims = x.get_shape()._dims
-    if dims is not None:
-        return len(dims)
-    return None
-
-
-def _sum(x, axis=None, keepdims=False):
-    '''Sum of the values in a tensor, alongside the specified axis.
-    '''
-    axis = _normalize_axis(axis, ndim(x))
-    return tf.reduce_sum(x, reduction_indices=axis, keep_dims=keepdims)
-
-
-def categorical_crossentropy(output, target, from_logits=False):
-    '''Categorical crossentropy between an output tensor
-    and a target tensor, where the target is a tensor of the same
-    shape as the output.
-    '''
-    # Note: tf.nn.softmax_cross_entropy_with_logits
-    # expects logits, Keras expects probabilities.
-    if not from_logits:
-        # scale preds so that the class probas of each sample sum to 1
-        output /= tf.reduce_sum(output,
-                                reduction_indices=len(output.get_shape()) - 1,
-                                keep_dims=True)
-        # manual computation of crossentropy
-        epsilon = _to_tensor(config._EPSILON, output.dtype.base_dtype)
-        output = tf.clip_by_value(output, epsilon, 1. - epsilon)
-        return - tf.reduce_sum(target * tf.log(output),
-                               reduction_indices=len(output.get_shape()) - 1)
-    else:
-        return tf.nn.softmax_cross_entropy_with_logits(output, target)
-
-
-def customized_categorical_crossentropy(y_true, y_pred):
-    # print "ce",K.sum(K.categorical_crossentropy(y_pred, y_true)).get_shape()
-    # print K.categorical_crossentropy(y_pred, y_true).get_shape()
-    return _sum(categorical_crossentropy(y_pred, y_true))
-
-
-def concatenate(tensors, nm,axis=-1):
-    '''Concantes a list of tensors alongside the specified axis.
-    '''
-    if axis < 0:
-        if len(tensors[0].get_shape()):
-            axis = axis % len(tensors[0].get_shape())
-        else:
-            axis = 0
-    return tf.concat(axis, tensors,name=nm)
-
-
-
+#
+# def _normalize_axis(axis, ndim):
+#     if type(axis) is tuple:
+#         axis = list(axis)
+#     if type(axis) is list:
+#         for i, a in enumerate(axis):
+#             if a is not None and a < 0:
+#                 axis[i] = a % ndim
+#     else:
+#         if axis is not None and axis < 0:
+#             axis = axis % ndim
+#     return axis
+#
+#
+# def ndim(x):
+#     '''Returns the number of axes in a tensor, as an integer.
+#     '''
+#     dims = x.get_shape()._dims
+#     if dims is not None:
+#         return len(dims)
+#     return None
+#
+#
+# def _sum(x, axis=None, keepdims=False):
+#     '''Sum of the values in a tensor, alongside the specified axis.
+#     '''
+#     axis = _normalize_axis(axis, ndim(x))
+#     return tf.reduce_sum(x, reduction_indices=axis, keep_dims=keepdims)
+#
+#
+# def categorical_crossentropy(output, target, from_logits=False):
+#     '''Categorical crossentropy between an output tensor
+#     and a target tensor, where the target is a tensor of the same
+#     shape as the output.
+#     '''
+#     # Note: tf.nn.softmax_cross_entropy_with_logits
+#     # expects logits, Keras expects probabilities.
+#     if not from_logits:
+#         # scale preds so that the class probas of each sample sum to 1
+#         output /= tf.reduce_sum(output,
+#                                 reduction_indices=len(output.get_shape()) - 1,
+#                                 keep_dims=True)
+#         # manual computation of crossentropy
+#         epsilon = _to_tensor(config._EPSILON, output.dtype.base_dtype)
+#         output = tf.clip_by_value(output, epsilon, 1. - epsilon)
+#         return - tf.reduce_sum(target * tf.log(output),
+#                                reduction_indices=len(output.get_shape()) - 1)
+#     else:
+#         return tf.nn.softmax_cross_entropy_with_logits(output, target)
+#
+#
+# def customized_categorical_crossentropy(y_true, y_pred):
+#     # print "ce",K.sum(K.categorical_crossentropy(y_pred, y_true)).get_shape()
+#     # print K.categorical_crossentropy(y_pred, y_true).get_shape()
+#     return _sum(categorical_crossentropy(y_pred, y_true))
+#
+#
+# def concatenate(tensors, nm,axis=-1):
+#     '''Concantes a list of tensors alongside the specified axis.
+#     '''
+#     if axis < 0:
+#         if len(tensors[0].get_shape()):
+#             axis = axis % len(tensors[0].get_shape())
+#         else:
+#             axis = 0
+#     return tf.concat(axis, tensors,name=nm)
+#
+#
+#
