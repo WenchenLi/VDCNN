94c543acbf99aacb68be7abcbdfb5f1022283ae3
WenchenLi <largemaths@gmail.com>
2017-03-27 09:37:06 -0500

    clean up a bit

--- main.py
+++ main.py
@@ -16,13 +16,12 @@
 run the training/dev/testing
 """
 import tensorflow as tf
-import time
 import numpy as np
 import os
 import time
 import datetime
 import util
-from model import TextCNN,VDCNN
+from model import VDCNN
 from tensorflow.contrib import learn
 import config
 
@@ -101,7 +100,7 @@ with tf.Graph().as_default():
     is_training = tf.placeholder('bool', [], name='is_training')
 
     with sess.as_default():
-        cnn = VDCNN(
+        vdcnn = VDCNN(
             sequence_length=x_train.shape[1], #x_train.shape[1]
             num_classes=y_train.shape[1],
             vocab_size=len(vocab_processor.vocabulary_),
@@ -112,7 +111,7 @@ with tf.Graph().as_default():
         # Define Training procedure
         global_step = tf.Variable(0, name="global_step", trainable=False)
         optimizer = tf.train.AdamOptimizer(1e-3)
-        grads_and_vars = optimizer.compute_gradients(cnn.loss)
+        grads_and_vars = optimizer.compute_gradients(vdcnn.loss)
         train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
 
         # Keep track of gradient values and sparsity (optional)
@@ -131,8 +130,8 @@ with tf.Graph().as_default():
         print("Writing to {}\n".format(out_dir))
 
         # Summaries for loss and accuracy
-        loss_summary = tf.summary.scalar("loss", cnn.loss)
-        acc_summary = tf.summary.scalar("accuracy", cnn.accuracy)
+        loss_summary = tf.summary.scalar("loss", vdcnn.loss)
+        acc_summary = tf.summary.scalar("accuracy", vdcnn.accuracy)
 
         # Train Summaries
         train_summary_op = tf.summary.merge([loss_summary, acc_summary, grad_summaries_merged])
@@ -162,15 +161,15 @@ with tf.Graph().as_default():
             A single training step
             """
             feed_dict = {
-              cnn.input_x: x_batch,
-              cnn.input_y: y_batch,
+              vdcnn.input_x: x_batch,
+              vdcnn.input_y: y_batch,
               is_training: True
             }
 
-            #   cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
+            #   vdcnn.dropout_keep_prob: FLAGS.dropout_keep_prob
             # }
             _, step, summaries, loss, accuracy = sess.run(
-                [train_op, global_step, train_summary_op, cnn.loss, cnn.accuracy],
+                [train_op, global_step, train_summary_op, vdcnn.loss, vdcnn.accuracy],
                 feed_dict)
             time_str = datetime.datetime.now().isoformat()
             print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
@@ -181,14 +180,14 @@ with tf.Graph().as_default():
             Evaluates model on a dev set
             """
             feed_dict = {
-              cnn.input_x: x_batch,
-              cnn.input_y: y_batch,
-              is_training:True
+              vdcnn.input_x: x_batch,
+              vdcnn.input_y: y_batch,
+              is_training:False
             }
-            #   cnn.dropout_keep_prob: 1.0
+            #   vdcnn.dropout_keep_prob: 1.0
             # }
             step, summaries, loss, accuracy = sess.run(
-                [global_step, dev_summary_op, cnn.loss, cnn.accuracy],
+                [global_step, dev_summary_op, vdcnn.loss, vdcnn.accuracy],
                 feed_dict)
             time_str = datetime.datetime.now().isoformat()
             print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
@@ -210,147 +209,3 @@ with tf.Graph().as_default():
             if current_step % FLAGS.checkpoint_every == 0:
                 path = saver.save(sess, checkpoint_prefix, global_step=current_step)
                 print("Saved model checkpoint to {}\n".format(path))
-
-
-# def save_check_point(mod_arg, mod_aux, pre, epoch):
-#     """
-#     Save model each epoch, load as:
-#
-#     sym, arg_params, aux_params = \
-#         mx.model.load_checkpoint(model_prefix, n_epoch_load)
-#
-#     # assign parameters
-#     mod.set_params(arg_params, aux_params)
-#
-#     OR
-#
-#     mod.fit(..., arg_params=arg_params, auxdata_helpers
-# data_helpers_params=aux_params,
-#             begin_epoch=n_epoch_load)
-#     """
-#
-#     save_dict = {('arg:%s' % k): v for k, v in mod_arg.items()}
-#     save_dict.update({('aux:%s' % k): v for k, v in mod_aux.items()})
-#     param_name = '%s-%04d.pk' % (pre, epoch)
-#     pickle.dump(save_dict, open(param_name, "wb"))
-#     print('Saved checkpoint to \"%s\"' % param_name)
-#
-#
-# def load_check_point(file_name):
-#     # Load file
-#     print(file_name)
-#     save_dict = pickle.load(open(file_name, "rb"))
-#     # Extract data from save
-#     arg_params = {}
-#     aux_params = {}
-#     for k, v in save_dict.items():
-#         tp, name = k.split(':', 1)
-#         if tp == 'arg':
-#             arg_params[name] = v
-#         if tp == 'aux':
-#             aux_params[name] = v
-#
-#     # Recreate model
-#     cnn = create_vdcnn()
-#     mod = mx.mod.Module(cnn, context=ctx)
-#
-#     # Bind shape
-#     mod.bind(data_shapes=[('data', DATA_SHAPE)],
-#              label_shapes=[('softmax_label', (BATCH_SIZE,))])
-#
-#     # assign parameters from save
-#     mod.set_params(arg_params, aux_params)
-#     print('Model loaded from disk')
-#
-#     return mod
-#
-#
-# def train_model(train_fname):
-#     # Create mx.mod.Module()
-#     cnn = create_vdcnn()
-#     mod = mx.mod.Module(cnn, context=ctx)
-#
-#     # Bind shape
-#     mod.bind(data_shapes=[('data', DATA_SHAPE)],
-#              label_shapes=[('softmax_label', (BATCH_SIZE,))])
-#
-#     # Initialise parameters and optimiser
-#     mod.init_params(mx.init.Normal(sigma=SD))
-#     mod.init_optimizer(optimizer='sgd',
-#                        optimizer_params={
-#                            "learning_rate": 0.01,
-#                            "momentum": 0.9,
-#                            "wd": 0.00001,
-#                            "rescale_grad": 1.0 / BATCH_SIZE
-#                        })
-#
-#     # Load Data
-#     X_train, y_train = load_file('amazon_review_polarity_train.csv')
-#
-#     # Train
-#     print("Alphabet %d characters: " % len(ALPHABET), ALPHABET)
-#     print("started training")
-#     tic = time.time()
-#
-#     # Evaluation metric:
-#     metric = mx.metric.Accuracy()
-#
-#     # Train EPOCHS
-#     for epoch in range(EPOCHS):
-#         t = 0
-#         metric.reset()
-#         tic_in = time.time()
-#         for batch in load_data_frame(X_data=X_train,
-#                                      y_data=y_train,
-#                                      batch_size=BATCH_SIZE,
-#                                      shuffle=True):
-#             # Push data forwards and update metric
-#             mod.forward_backward(batch)
-#             mod.update()
-#             mod.update_metric(metric, batch.label)
-#
-#             # For training + testing
-#             # mod.forward(batch, is_train=True)
-#             # mod.update_metric(metric, batch.label)
-#             # Get weights and update
-#             # For training only
-#             # mod.backward()
-#             # mod.update()
-#             # Log every 50 batches = 128*50 = 6400
-#             t += 1
-#             if t % 50 == 0:
-#                 train_t = time.time() - tic_in
-#                 metric_m, metric_v = metric.get()
-#                 print("epoch: %d iter: %d metric(%s): %.4f dur: %.0f" % (epoch, t, metric_m, metric_v, train_t))
-#
-#         # Checkpoint
-#         arg_params, aux_params = mod.get_params()
-#         save_check_point(mod_arg=arg_params,
-#                          mod_aux=aux_params,
-#                          pre=train_fname,
-#                          epoch=epoch)
-#         print("Finished epoch %d" % epoch)
-#
-#     print("Done. Finished in %.0f seconds" % (time.time() - tic))
-#
-#
-# def test_model(test_fname):
-#     # Load saved model:
-#     mod = load_check_point(test_fname)
-#     # assert mod.binded and mod.params_initialized
-#
-#     # Load data
-#     X_test, y_test = load_file('amazon_review_polarity_test.csv')
-#
-#     # Score accuracy
-#     metric = mx.metric.Accuracy()
-#
-#     # Test batches
-#     for batch in load_data_frame(X_data=X_test,
-#                                  y_data=y_test,
-#                                  batch_size=len(y_test)):
-#         mod.forward(batch, is_train=False)
-#         mod.update_metric(metric, batch.label)
-#
-#         metric_m, metric_v = metric.get()
-#         print("TEST(%s): %.4f" % (metric_m, metric_v))
